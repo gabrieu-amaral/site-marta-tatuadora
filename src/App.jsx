a64d9cfb0fff757a724b6c1f30806be2fc6186f7
Gabriel Amaral <gabriel100j@hotmail.com>
2022-07-21 17:50:20 -0300

    video bg, responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -6,79 +6,71 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-       <nav class="fixed top-0 left-0 w-full shadow-xl backdrop-blur-md z-20">
-                <div class="container m-auto flex justify-between items-center text-cyan-100"> 
-                     <a class="p-6" href="">Marta Tattoo</a>
-                
-                    <ul class="hidden md:flex items-center pr-10 text-base font-semibold cursor-pointer">
-                        <li class="hover:bg-blue-800 py-4 px-6">
-                                <a href="#home">Home</a>
-                        </li>
+    <div className="App" class="bg-zinc-800 h-full w-full m-0 box-border">
+       <nav class="fixed top-0 left-0 w-full shadow-xl backdrop-blur-lg z-20">
+          <div class="container m-auto flex justify-between items-center text-gray-100"> 
+                <a class="p-6" href="">Marta Tattoo</a>
+          
+              <ul class="hidden md:flex items-center pr-10 text-base font-semibold cursor-pointer">
+                  <li class="hover:bg-blue-800 py-4 px-6">
+                          <a href="#home">Home</a>
+                  </li>
 
-                        <li class="hover:bg-blue-800 py-4 px-6">
-                                <a href="#sobre">Sobre</a>
-                        </li>
+                  <li class="hover:bg-blue-800 py-4 px-6">
+                          <a href="#sobre">Sobre</a>
+                  </li>
 
-                        <li class="hover:bg-blue-800py-4 px-6">
-                                <a href="#missao">Flash's</a>
-                        </li>
+                  <li class="hover:bg-blue-800py-4 px-6">
+                          <a href="#missao">Flash's</a>
+                  </li>
 
-                        <li class="hover:bg-blue-800 py-4 px-6">
-                                <a href="#footer">Contato</a>
-                        </li>
-                    </ul>
+                  <li class="hover:bg-blue-800 py-4 px-6">
+                          <a href="#footer">Contato</a>
+                  </li>
+              </ul>
 
+              {/*mobile menu*/}
 
-                
+              <button class="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-blue-800 group">
+                  <div class="w-6 h-0.5 bg-gray-200 mb-1"></div>
+                  <div class="w-6 h-0.5 bg-gray-200 mb-1"></div>
+                  <div class="w-6 h-0.5 bg-gray-200"></div>
+                  <div class="absolute top-0 -right-full h-screen w-8/12 bg-zinc-800 backdrop-blur-xl border-none opacity-0
+                  group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
+                      <ul class="flex flex-col items-center w-full text-base cursor-pointer pt-10">
+                          <li class="hover:bg-blue-800 py-4 px-6">
+                                  <a href="#home">Home</a>
+                          </li>
 
-                    <button class="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-blue-800 group">
-                        <div class="w-6 h-0.5 bg-gray-200 mb-1"></div>
-                        <div class="w-6 h-0.5 bg-gray-200 mb-1"></div>
-                        <div class="w-6 h-0.5 bg-gray-200"></div>
-                        <div class="absolute top-0 -right-full h-screen w-8/12 bg-zinc-800 backdrop-blur-md border-none opacity-0
-                        group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
-                            <ul class="flex flex-col items-center w-full text-base cursor-pointer pt-10">
-                                <li class="hover:bg-blue-800 py-4 px-6">
-                                        <a href="#home">Home</a>
-                                </li>
+                          <li class="hover:bg-blue-800 py-4 px-6">
+                                  <a href="#sobre">Sobre</a>
+                          </li>
 
-                                <li class="hover:bg-blue-800 py-4 px-6">
-                                        <a href="#sobre">Sobre</a>
-                                </li>
+                          <li class="hover:bg-blue-800 py-4 px-6">
+                                  <a href="#missao">Flash's</a>
+                          </li>
 
-                                <li class="hover:bg-blue-800 py-4 px-6">
-                                        <a href="#missao">Flash's</a>
-                                </li>
-
-                                <li class="hover:bg-blue-800 py-4 px-6">
-                                        <a href="#footer">Contato</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </button>
-                </div>
+                          <li class="hover:bg-blue-800 py-4 px-6">
+                                  <a href="#footer">Contato</a>
+                          </li>
+                      </ul>
+                  </div>
+              </button>
+          </div>
         </nav>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" >
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      
+      <video src="../public/video-site2.mp4" muted loop autoPlay class=" w-full min-w-[800px] absolute top-0 z-0"></video>
+        
+      <div class="container flex-col mx-auto text z-40 relative h-auto align-middle lg:pt-96 pt-28 lg:pl-5">
+        <h1 class="text-5xl lg:text-8xl text-cyan-50 font-extrabold px-6 md:p-0 tracking-wide">Levando arte a sério</h1>
+        <p class="text-3xl text-cyan-50 px-6 font-extralight md:p-0 lg:mt-5 my-5 tracking-wide">tatuando com excelência desde 2018.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div class="container relative flex m-auto h-96 z-60 bg-zinc-900 md:rounded-3xl lg:mt-80 md:mt-40 mt-16 shadow-xl">
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </div>
   )
 }
